2c486b218bba4a2287b7fc18098b00d3e3865ec3
Thiago <thi_wn@hotmail.com>
2019-07-23 11:38:18 -0300

    Clearing up request code

--- main/hooks.js
+++ main/hooks.js
@@ -2,13 +2,20 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
 const useArticles = (query) => {
-    const [data, setData] = useState(null)
+    let baseURL = 'https://newsapi.org/v2'
+    let apiKey = '27e16724ac59410ba06aab272354fbdb'
 
+    const [data, setData] = useState(null)
+    
     useEffect( () => {
         const fetch = async () => {
-            let response = await axios.get(`https://newsapi.org/v2/everything?q=${query}&from=2019-06-22&sortBy=publishedAt&apiKey=27e16724ac59410ba06aab272354fbdb`)
-            let { articles } = await response.data
-            setData(articles)
+            try {
+                let response = await axios.get(`${baseURL}/everything?q=${query}&sortBy=publishedAt&apiKey=${apiKey}`)
+                let { articles } = await response.data
+                setData(articles)
+            } catch (error) {
+                console.log(error)
+            }
         }
 
         fetch();
